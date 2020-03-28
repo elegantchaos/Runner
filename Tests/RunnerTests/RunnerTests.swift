eb61fb5efb89389700559c6dd74434451789d2c6
Sam Deane <sam@elegantchaos.com>
2020-03-28 17:13:01 +0000

    added tests of stderr capture, non-zero status return

--- Tests/RunnerTests/RunnerTests.swift
+++ Tests/RunnerTests/RunnerTests.swift
@@ -4,13 +4,24 @@ import XCTestExtensions
 @testable import Runner
 
 final class RunnerTests: XCTestCase {
-    func testSync() {
-        let runner = Runner(for: URL(fileURLWithPath: "/usr/bin/which"))
-        let result = try! runner.sync(arguments: ["ls"])
+    func testSyncZeroStatus() {
+        let url = testURL(named: "zero-status", withExtension: "sh")
+        let runner = Runner(for: url)
+        let result = try! runner.sync()
         XCTAssertEqual(result.status, 0)
-        XCTAssertEqual(result.stdout, "/bin/ls\n")
+        XCTAssertEqual(result.stdout, "stdout")
+        XCTAssertEqual(result.stderr, "stderr")
     }
-    
+
+    func testSyncNonZeroStatus() {
+        let url = testURL(named: "non-zero-status", withExtension: "sh")
+        let runner = Runner(for: url)
+        let result = try! runner.sync()
+        XCTAssertEqual(result.status, 123)
+        XCTAssertEqual(result.stdout, "stdout")
+        XCTAssertEqual(result.stderr, "stderr")
+    }
+
     func testLongRunning() {
         let url = testURL(named: "long-running", withExtension: "sh")
         let runner = Runner(for: url)
