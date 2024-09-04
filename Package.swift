66c6cf6017bf9704f5afab38e7a441e5a2ff42dd
Sam Deane <sam@elegantchaos.com>
2024-09-04 12:53:02 +0100

    Newer runner

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
   ],
 
   dependencies: [
-    .package(url: "https://github.com/elegantchaos/ChaosByteStreams", from: "1.0.0")
+    .package(url: "https://github.com/elegantchaos/ChaosByteStreams", from: "1.0.1")
   ],
 
   targets: [
